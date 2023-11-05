73625bb3b46edc17f7d2743aeb7cdc37dcdb40c4
Alice <114536079+AliceC0874203@users.noreply.github.com>
2023-11-05 17:12:04 -0500

    Update CreateTaskScreen.js

--- CreateTaskScreen.js
+++ CreateTaskScreen.js
@@ -1,13 +1,14 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text, TextInput, Button, StyleSheet, Alert } from 'react-native';
+import { View, StyleSheet, Alert, ScrollView } from 'react-native';
+import { Button, TextField, Picker, Text } from 'react-native-ui-lib';
 import DateTimePicker from '@react-native-community/datetimepicker';
-import RNPickerSelect from 'react-native-picker-select';
 import { firebase, auth, database } from './firebase';
 import { ref, push, set, query, orderByChild, equalTo, get } from 'firebase/database';
 import commonStyles from './style';
+import RNPickerSelect from 'react-native-picker-select';
 
 const CreateTaskScreen = ({ navigation, route }) => {
-  const {tasks, setTasks} = route.params;
+  const { tasks, setTasks } = route.params;
   const [taskName, setTaskName] = useState('');
   const [taskDescription, setTaskDescription] = useState('');
   const [taskStartDate, setTaskStartDate] = useState(new Date());
@@ -27,33 +28,33 @@ const CreateTaskScreen = ({ navigation, route }) => {
       setSelectedProject(projectObj);
       setTasks(tasks)
     }
-  }, [projectObj,tasks]);
+  }, [projectObj, tasks]);
 
   useEffect(() => {
     if (!project) {
-    // Fetch projects related to the current user and populate projectOptions
-    if (auth.currentUser) {
-      const currentUserEmail = auth.currentUser.email;
-      const userProjectsQuery = query(
-        ref(database, 'projects'),
-        orderByChild('user'),
-        equalTo(currentUserEmail)
-      );
+      // Fetch projects related to the current user and populate projectOptions
+      if (auth.currentUser) {
+        const currentUserEmail = auth.currentUser.email;
+        const userProjectsQuery = query(
+          ref(database, 'projects'),
+          orderByChild('user'),
+          equalTo(currentUserEmail)
+        );
 
-      get(userProjectsQuery)
-        .then((snapshot) => {
-          if (snapshot.exists()) {
-            const projects = snapshot.val();
-            const options = Object.keys(projects).map((projectId) => ({
-              label: projects[projectId].name,
-              value: projects[projectId],
-            }));
-            setProjectOptions(options);
-          }
-        })
-        .catch((error) => {
-          console.error('Error fetching projects:', error);
-        });
+        get(userProjectsQuery)
+          .then((snapshot) => {
+            if (snapshot.exists()) {
+              const projects = snapshot.val();
+              const options = Object.keys(projects).map((projectId) => ({
+                label: projects[projectId].name,
+                value: projects[projectId],
+              }));
+              setProjectOptions(options);
+            }
+          })
+          .catch((error) => {
+            console.error('Error fetching projects:', error);
+          });
       }
 
     }
@@ -109,11 +110,11 @@ const CreateTaskScreen = ({ navigation, route }) => {
       taskCost: 0,
       projectId: selectedProject ? selectedProject.projectId : null,
       status: 'New',
-      taskId : taskId,
-      owner : currentUserEmail,
-      member : selectedUser ? selectedUser.email.toLowerCase() : null,
-      actualEndDate : null,
-      noOfHours : 0
+      taskId: taskId,
+      owner: currentUserEmail,
+      member: selectedUser ? selectedUser.email.toLowerCase() : null,
+      actualEndDate: null,
+      noOfHours: 0
     };
 
     set(tasksRef, newTask)
@@ -135,15 +136,7 @@ const CreateTaskScreen = ({ navigation, route }) => {
 
   React.useLayoutEffect(() => {
     navigation.setOptions({
-      headerLeft: () => (
-        <View style={commonStyles.customHeaderLeft}>
-          <Button
-            onPress={() => navigation.goBack()}
-            title="< Back"
-            color="#007BFF"
-          />
-        </View>
-      ),
+      headerBackTitle: "Back",
       headerTitle: () => (
         <View style={commonStyles.customHeader}>
           <Text style={commonStyles.headerTitle}>Create a Task</Text>
@@ -152,98 +145,169 @@ const CreateTaskScreen = ({ navigation, route }) => {
     });
   }, [navigation]);
 
+  // Render function for the Date Picker
+  const renderDatePicker = (date, setDate, showDatePicker, setShowDatePicker) => (
+    <>
+      <Button
+        label={taskStartDate.toDateString()}
+        onPress={() => setShowStartDatePicker(true)}
+        style={styles.dateButton}
+        labelStyle={styles.dateLabel} // Apply the custom label style here
+      />
+      {showDatePicker && (
+        <DateTimePicker
+          value={date}
+          mode="date"
+          display="default"
+          onChange={(event, selectedDate) => {
+            if (selectedDate) {
+              setDate(selectedDate);
+            }
+            setShowDatePicker(false);
+          }}
+          minimumDate={new Date()}
+        />
+      )}
+    </>
+  );
 
   return (
-    <View style={styles.container}>
-      <TextInput
-        style={styles.input}
-        placeholder="Name"
-        value={taskName}
-        onChangeText={setTaskName}
-      />
-      <TextInput
-        style={[styles.input, { height: 4 * 40 }]} 
-        placeholder="Description"
-        value={taskDescription}
-        onChangeText={setTaskDescription}
-        multiline={true}
-        numberOfLines={4}
-      />
-      <Text>Start Date:</Text>
-      <Button
-        title={taskStartDate.toISOString().split('T')[0]}
-        onPress={() => setShowStartDatePicker(true)}
-      />
-      {showStartDatePicker && (
-        <DateTimePicker
-          value={taskStartDate}
-          mode="date"
-          display="default"
-          onChange={(event, selectedDate) => {
-            if (selectedDate) {
-              setTaskStartDate(selectedDate);
-            }
-            setShowStartDatePicker(false);
-          }}
-          minimumDate={new Date()} // Disallow past dates
+    <ScrollView style={styles.container}>
+      <View style={styles.fieldContainer}>
+        <TextField
+          placeholder="Enter task name"
+          value={taskName}
+          onChangeText={setTaskName}
+          style={styles.input}
         />
-      )}
-      <Text>End Date:</Text>
-      <Button
-        title={taskEndDate.toISOString().split('T')[0]}
-        onPress={() => setShowEndDatePicker(true)}
-      />
-      {showEndDatePicker && (
-        <DateTimePicker
-          value={taskEndDate}
-          mode="date"
-          display="default"
-          onChange={(event, selectedDate) => {
-            if (selectedDate) {
-              setTaskEndDate(selectedDate);
-            }
-            setShowEndDatePicker(false);
-          }}
-          minimumDate={new Date()} // Disallow past dates
+      </View>
+
+      <View style={styles.fieldContainer}>
+        <TextField
+          placeholder="Enter task description"
+          value={taskDescription}
+          onChangeText={setTaskDescription}
+          style={[styles.input, styles.multilineInput]}
+          multiline={true}
         />
-      )}
+      </View>
 
-<Text>Assign to:</Text>
+      <View style={styles.fieldContainer}>
+        <Text style={styles.label}>Start Date</Text>
+        {renderDatePicker(taskStartDate, setTaskStartDate, showStartDatePicker, setShowStartDatePicker)}
+      </View>
 
-  <RNPickerSelect
-    items={userOptions}
-    onValueChange={(value) => setSelectedUser(value)}
-    // Disable the picker if a project is selected
-  />
+      <View style={styles.fieldContainer}>
+        <Text style={styles.label}>End Date</Text>
+        {renderDatePicker(taskEndDate, setTaskEndDate, showEndDatePicker, setShowEndDatePicker)}
+      </View>
 
-<Text>Project:</Text>
-{project ? (
-  <Text style={styles.selectedProjectText}>
-    {project.name}
-  </Text>
-) : (
-  <RNPickerSelect
-    items={projectOptions}
-    onValueChange={(value) => setSelectedProject(value)}
-    disabled={project ? true : false} // Disable the picker if a project is selected
-  />
-)}
-      <Button title="Save" onPress={handleCreateTask} />
-    </View>
+      <View style={styles.fieldContainer}>
+        <Text style={styles.label}>Assign to</Text>
+        <RNPickerSelect
+          onValueChange={(value) => setSelectedUser(value)}
+          items={userOptions}
+          style={pickerSelectStyles}
+          placeholder={{
+            label: 'Select a member',
+            value: null,
+          }}
+          value={selectedUser}
+        />
+
+
+      </View>
+
+      <View style={styles.fieldContainer}>
+        <Text style={styles.label}>Project</Text>
+        {project ? (
+          <Text style={styles.selectedProjectText}>{project.name}</Text>
+        ) : (
+          <RNPickerSelect
+            onValueChange={(value) => setSelectedProject(value)}
+            items={projectOptions}
+            style={pickerSelectStyles}
+            placeholder={{
+              label: 'Select a project',
+              value: null,
+            }}
+            value={selectedProject}
+          />
+        )}
+      </View>
+
+      <Button label="Save Task" onPress={handleCreateTask} style={styles.saveButton} />
+    </ScrollView>
   );
 };
+
+const pickerSelectStyles = StyleSheet.create({
+  inputIOS: {
+    fontSize: 16,
+    paddingVertical: 12,
+    paddingHorizontal: 10,
+    borderWidth: 1,
+    borderColor: 'gray',
+    borderRadius: 20,
+    color: 'black',
+    paddingRight: 30, // to ensure the text is never behind the icon
+  },
+  inputAndroid: {
+    fontSize: 16,
+    paddingHorizontal: 10,
+    paddingVertical: 8,
+    borderWidth: 0.5,
+    borderColor: 'purple',
+    borderRadius: 20,
+    color: 'black',
+    paddingRight: 30, // to ensure the text is never behind the icon
+  },
+});
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     padding: 16,
+    backgroundColor: '#fff',
+  },
+  fieldContainer: {
+    marginBottom: 20,
+  },
+  label: {
+    fontSize: 16,
+    color: '#333',
+    marginBottom: 5,
   },
   input: {
     height: 40,
+    width: '100%', // Set a fixed width for the TextFields
     borderColor: 'gray',
     borderWidth: 1,
-    marginBottom: 10,
+    borderRadius: 10, // This is half of the height to make the TextField rounded
     paddingHorizontal: 10,
+    marginBottom: 20,
+  },
+  multilineInput: {
+    minHeight: 100, // Minimum height for text area
+  },
+  dateButton: {
+    backgroundColor: '#fff', // Assuming you want a white background
+    borderWidth: 1,
+    borderColor: 'gray',
+    borderRadius: 20,
+    padding: 10,
+    justifyContent: 'flex-start', // Aligns button content (icon and text) to the left
+    height: 40, // Set a fixed height
+  },
+  dateLabel: {
+    color: 'black', // Sets the font color to black
+    fontSize: 16, // Sets the font size
+    textAlign: 'left', // Aligns the text to the left
+  },
+  saveButton: {
+    marginTop: 20,
+    borderRadius: 20,
+    backgroundColor: '#5848ff', // Purple color from your tab bar design
   },
 });
 
