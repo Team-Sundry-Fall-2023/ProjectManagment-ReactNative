7bfc2efd8b829af45c2b05a98447edf8dea0eeaa
Alice <114536079+AliceC0874203@users.noreply.github.com>
2023-11-06 00:30:34 -0500

    Update ProjectDetailScreen.js

--- ProjectDetailScreen.js
+++ ProjectDetailScreen.js
@@ -1,7 +1,8 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text, Button, FlatList, StyleSheet,Alert } from 'react-native';
-import { firebase ,auth, database} from './firebase';
-import {  ref, query, orderByChild, equalTo, get} from "firebase/database";
+import { View, Text, TouchableOpacity, FlatList, StyleSheet, Alert } from 'react-native';
+import { Button } from 'react-native-ui-lib';
+import { firebase, auth, database } from './firebase';
+import { ref, query, orderByChild, equalTo, get } from "firebase/database";
 
 const ProjectDetailScreen = ({ route, navigation }) => {
   const { projectObj } = route.params;
@@ -19,67 +20,90 @@ const ProjectDetailScreen = ({ route, navigation }) => {
 
       console.log('project ', project);
       const tasksList = [];
-    const userQuery = query(ref(database, 'tasks'),orderByChild('projectId'),equalTo(project.projectId) );
-    console.log('userQuery' + userQuery )
-     get(userQuery).then((snapshot) => {
-         if (snapshot.exists()) {
-           // The snapshot contains the user data matching the email
-           const tasks = snapshot.val();
+      const userQuery = query(ref(database, 'tasks'), orderByChild('projectId'), equalTo(project.projectId));
+      console.log('userQuery' + userQuery)
+      get(userQuery).then((snapshot) => {
+        if (snapshot.exists()) {
+          // The snapshot contains the user data matching the email
+          const tasks = snapshot.val();
 
-           Object.keys(tasks).forEach((taskId) => {
+          Object.keys(tasks).forEach((taskId) => {
             const task = tasks[taskId];
             console.log('task item', task);
             tasksList.push(task);
-             
+
           });
-          console.log('taskList ' + tasksList.length )
+          console.log('taskList ' + tasksList.length)
           setTasks(tasksList);
-         } else {
+        } else {
           setTasks(tasksList);
-         }
-       }).catch((error) => {
+        }
+      }).catch((error) => {
         setTasks(tasksList);
-        showAlert('Error','Error finding Tasks :', error.message);
-         return null;
-       });
-      }
+        showAlert('Error', 'Error finding Tasks :', error.message);
+        return null;
+      });
+    }
 
 
   }, [project]);
 
+  const formatDate = (dateString) => {
+    if (!dateString) {
+      return 'N/A';
+    }
+    const date = new Date(dateString);
+    if (isNaN(date)) {
+      return 'Invalid date';
+    }
+    const options = {
+      year: 'numeric', month: 'long', day: 'numeric',
+      hour: '2-digit', minute: '2-digit', timeZoneName: 'short'
+    };
+    return new Intl.DateTimeFormat('en-US', options).format(date);
+  };
+
+  const renderTaskItem = ({ item }) => (
+    <View style={styles.taskItem}>
+      <Text style={styles.taskName}>{item.taskName}</Text>
+      <Text style={styles.taskDescription}>{item.taskDescription}</Text>
+      <View style={styles.taskDetails}>
+        <Text style={styles.taskDetailText}>Start: {formatDate(item.taskStartDate)}</Text>
+        <Text style={styles.taskDetailText}>End: {formatDate(item.taskEndDate)}</Text>
+        <Text style={styles.taskDetailText}>Status: {item.status}</Text>
+        <Text style={styles.taskDetailText}>Member: {item.member}</Text>
+        <Text style={styles.taskDetailText}>Actual End: {formatDate(item.actualEndDate)}</Text>
+        <Text style={styles.taskDetailText}>Cost: {item.taskCost}</Text>
+        <Text style={styles.taskDetailText}>Hours: {item.noOfHours}</Text>
+      </View>
+    </View>
+  );
+
   return (
     <View style={styles.container}>
       {project && (
-        <View>
-          <Text style={styles.header}>Project Name: {project.name}</Text>
-          <Text style={styles.header}>Project Description: {project.description}</Text>
-          <Text style={styles.header}>Project Cost: {project.projectCost}</Text>
-          <Button
-            title="Create Task"
-            onPress={() => navigation.navigate('CreateTask', {projectObj: project, tasks, setTasks })}
-          />
+        <>
+          <View style={styles.projectHeader}>
+            <Text style={styles.projectName}>{project.name}</Text>
+            <Text style={styles.projectDescription}>{project.description}</Text>
+            <Text style={styles.projectCost}>Cost: {project.projectCost}</Text>
+            <Button
+              label="Create Task"
+              onPress={() => navigation.navigate('CreateTask', { projectObj: project, tasks, setTasks })}
+              style={styles.createTaskButton}
+            />
+          </View>
           {tasks.length > 0 ? (
             <FlatList
               data={tasks}
-              keyExtractor={(item) => item.id}
-              renderItem={({ item }) => (
-                <View style={styles.taskItem}>
-                    <Text style={styles.taskName}>{item.taskName}</Text>
-                <Text style={styles.taskDescription}>{item.taskDescription}</Text>
-                <Text>{`Start Date: ${item.taskStartDate}`}</Text>
-                <Text>{`End Date: ${item.taskEndDate}`}</Text>
-                <Text>{`Status: ${item.status}`}</Text>
-                <Text>{`Member: ${item.member}`}</Text>
-                <Text>{`Actual End Date: ${item.actualEndDate}`}</Text>
-                <Text>{`Cost: ${item.taskCost}`}</Text>
-                <Text>{`Hours: ${item.noOfHours}`}</Text>
-                </View>
-              )}
+              keyExtractor={(item) => item.id?.toString() ?? ''}
+              renderItem={renderTaskItem}
+              showsVerticalScrollIndicator={false}
             />
           ) : (
-            <Text>No tasks found for this project.</Text>
+            <Text style={styles.noTasksText}>No tasks found for this project.</Text>
           )}
-        </View>
+        </>
       )}
     </View>
   );
@@ -92,25 +116,67 @@ const showAlert = (title, message) => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    padding: 16,
+    backgroundColor: '#f7f7f7',
   },
-  header: {
-    fontSize: 24,
-    marginBottom: 20,
+  projectHeader: {
+    padding: 16,
+    borderBottomWidth: 1,
+    borderBottomColor: '#e1e1e1',
+    backgroundColor: 'white',
+  },
+  projectName: {
+    fontSize: 22,
+    fontWeight: 'bold',
+    marginBottom: 8,
+  },
+  projectDescription: {
+    fontSize: 16,
+    color: '#333',
+    marginBottom: 8,
+  },
+  projectCost: {
+    fontSize: 16,
+    color: '#333',
+    marginBottom: 16,
+  },
+  createTaskButton: {
+    marginBottom: 16,
   },
   taskItem: {
-    borderWidth: 1,
-    borderColor: 'gray',
-    padding: 10,
-    marginBottom: 10,
+    backgroundColor: 'white',
+    borderRadius: 8,
+    padding: 16,
+    marginVertical: 8,
+    marginHorizontal: 16,
+    elevation: 2, // for Android shadow
+    shadowOpacity: 0.1, // for iOS shadow
+    shadowRadius: 4,
+    shadowColor: '#000',
+    shadowOffset: { height: 2, width: 0 },
+  },
+  taskDetails: {
+    marginTop: 8,
+  },
+  taskDetailText: {
+    fontSize: 14,
+    color: '#666',
+    marginBottom: 4,
   },
   taskName: {
     fontSize: 18,
     fontWeight: 'bold',
-    marginBottom: 5,
+    marginBottom: 4,
   },
   taskDescription: {
-    marginBottom: 5,
+    fontSize: 14,
+    color: '#666',
+    marginBottom: 4,
+  },
+  noTasksText: {
+    textAlign: 'center',
+    marginTop: 16,
+    fontSize: 16,
+    color: '#999',
   },
 });
 
